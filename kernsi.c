850fa26b39dc0cca26ddf37e1737aeedfc65acb2
Sreekanth Rupavatharam <rupavath@juniper.net>
2019-09-20 14:49:23 -0700

    First Commit

--- kernsi.c
+++ kernsi.c
@@ -4,11 +4,22 @@
  *
  * Description: Syscall intercept code for kernel module.
  *
- * Author: Rupavatharam, Sreekanth <rupavath@juniper.net>
+ * Author: Rupavatharam, Sreekanth rskanth@yahoo.com
  *
- * Copyright (c) 2018, Juniper Networks Inc.
- * All rights reserved.
-*/
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 
 #include<linux/init.h>
@@ -22,57 +33,115 @@
 #include<linux/syscalls.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("rupavath");
+MODULE_AUTHOR("rskanth@yahoo.com");
 
 unsigned long *ksi_syscall_table;
 
-asmlinkage int (*original_write)(unsigned int, const char __user *, size_t);
-asmlinkage int (*original_socket)(int, int, int);
+struct mysyscall {
+	void *orig_ptr;
+	void *new_ptr;
+	int num_syscall;
+};
 
-asmlinkage int new_write(unsigned int fd, const char __user *buf, size_t count){
-	//Hijacked write function here
-//	printk("%s: %d\n", __func__, __LINE__);
-	return (*original_write)(fd, buf, count);
-}
 
-asmlinkage int new_socket(int a, int b , int c)
+void *get_orig_fn(int syscall);
+asmlinkage int new_write(unsigned int fd, const char __user *buf, size_t count);
+asmlinkage int new_socket(int domain, int type , int protocol);
+static int kernsi_init_mod(void);
+static void kernsi_exit_mod(void);
+
+asmlinkage int new_write(unsigned int fd, const char __user *buf, size_t count)
 {
-	printk("%s - %s: %d\n", current->comm, __func__, __LINE__);
-    	return (*original_socket)(a, b, c);
+	asmlinkage int (*original_write)(unsigned int, const char __user *, size_t);
+
+	original_write = get_orig_fn(__NR_write);
+	printk_ratelimited(KERN_INFO
+				"write %s - fd: %d buf: %p count: %ld\n",
+		current->comm, fd, buf, count);
+	if(original_write)
+		return (*original_write)(fd, buf, count);
+	return -1;
 }
 
-void
-get_syscall_table(void)
+asmlinkage int new_socket(int domain, int type , int protocol)
+{
+	asmlinkage int (*original_socket)(int, int, int);
+	printk("%s - Domain: 0x%x, type: 0x%x, protocol: 0x%x\n", current->comm,
+		domain, type, protocol);
+	original_socket = get_orig_fn(__NR_socket);
+	if(original_socket)
+	    	return (*original_socket)(domain, type, protocol);
+	return -1;
+}
+
+#define MAX_SYSCALLS 10 /* XXX */
+
+/* Add the syscalls that you would want to hijack here */
+struct mysyscall hijack_calls[MAX_SYSCALLS] = {
+    				    { NULL, (void *)new_socket, __NR_socket},
+	    			    { NULL, (void *)new_write, __NR_write},
+			 	    { NULL, NULL, 0 },
+		 		};
+
+void *get_orig_fn(int syscall)
+{
+    	int i;
+    	for(i=0;i<MAX_SYSCALLS;i++) {
+	    if(hijack_calls[i].num_syscall == syscall)
+		return hijack_calls[i].orig_ptr;
+	}
+	return NULL;
+}
+
+void get_syscall_table(void)
 {
 	ksi_syscall_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
 	printk("ksi_syscall_table: %p\n", ksi_syscall_table);
 
 }
 
-static int kernsi_init_mod(void){
 
+static int kernsi_init_mod(void)
+{
+	int i;
+	struct mysyscall *entry;
     	get_syscall_table();
-	//Changing control bit to allow write
-	write_cr0 (read_cr0 () & (~ 0x10000));
-
-	original_write = (void *)ksi_syscall_table[__NR_write];
-	original_socket = (void *)ksi_syscall_table[__NR_socket];
 	if(ksi_syscall_table == NULL)
 	    return -1;
-	ksi_syscall_table[__NR_write] = (unsigned long) new_write;
-	ksi_syscall_table[__NR_socket] = (unsigned long) new_socket;
-	printk("Write system call old address: %p\n", original_write);
-	printk("Write system call new address: %p\n", new_write);
+	//Changing control bit to allow write
+	write_cr0 (read_cr0 () & (~ 0x10000));
+	for(i=0;i<MAX_SYSCALLS;i++) {
+	    	entry = &hijack_calls[i];
+		if(entry && entry->new_ptr != NULL) {
+			entry->orig_ptr = (void *)ksi_syscall_table[entry->num_syscall];
+			ksi_syscall_table[entry->num_syscall] = (unsigned long)entry->new_ptr;
+			printk("Changing ptr for %d from %p --> %p\n", entry->num_syscall,
+						entry->orig_ptr,
+						entry->new_ptr);
+		}
+
+	}
 	//Changing control bit back
 	write_cr0 (read_cr0 () | 0x10000);
 	return 0;
 }
 
-static void kernsi_exit_mod(void){
+static void kernsi_exit_mod(void)
+{
+	struct mysyscall *entry;
+	int i;
+
 	//Cleanup
 	write_cr0 (read_cr0 () & (~ 0x10000));
-	ksi_syscall_table[__NR_write] = (unsigned long)original_write;
-	ksi_syscall_table[__NR_socket] = (unsigned long)original_socket;
+	for(i=0;i<MAX_SYSCALLS;i++) {
+	    	entry = &hijack_calls[i];
+		if(entry->orig_ptr) {
+		    	printk("Restoring ptr for %d \n", entry->num_syscall);
+			ksi_syscall_table[entry->num_syscall] =
+			    	(unsigned long) entry->orig_ptr;
+
+		}
+	}
 	write_cr0 (read_cr0 () | 0x10000);
 	printk("Module exited cleanly");
 	return;
